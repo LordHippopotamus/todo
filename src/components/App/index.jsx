3b81ad810b7788b37bc74c6fb5e494e201cacd22
LordHippopotamus <LordHippopotamus@yandex.ru>
2021-10-09 19:34:01 +0300

    feat(App): create basic layout

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,8 +1,38 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import {
+  Container, Form, Button, ListGroup,
+} from 'react-bootstrap';
 
 export default function App() {
   return (
-    <Container />
+    <Container className="bg-light p-4 my-5">
+      <Form className="d-flex">
+        <Form.Control type="text" placeholder="What needs to be done?" className="me-2" />
+        <Button variant="primary" type="submit">
+          Add
+        </Button>
+      </Form>
+      <hr className="my-3" />
+      <ListGroup>
+        <ListGroup.Item className="d-flex align-items-center justify-content-between">
+          Todo item
+          <Button variant="outline-success">
+            Done
+          </Button>
+        </ListGroup.Item>
+        <ListGroup.Item className="d-flex align-items-center justify-content-between">
+          Todo item
+          <Button variant="outline-success">
+            Done
+          </Button>
+        </ListGroup.Item>
+        <ListGroup.Item className="d-flex align-items-center justify-content-between">
+          Todo item
+          <Button variant="outline-success">
+            Done
+          </Button>
+        </ListGroup.Item>
+      </ListGroup>
+    </Container>
   );
 }
