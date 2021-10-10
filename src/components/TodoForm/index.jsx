67dd99c442231412bc492daff58d51e4572d2c35
LordHippopotamus <LordHippopotamus@yandex.ru>
2021-10-10 11:04:04 +0300

    refactor(TodoForm): transform to controlled component

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -4,13 +4,30 @@ import { Form, Button } from 'react-bootstrap';
 export default class TodoForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: '',
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   render() {
+    const { name } = this.state;
+
     return (
       <Form className="d-flex">
-        <Form.Control type="text" placeholder="What needs to be done?" className="me-2" />
+        <Form.Control
+          type="text"
+          placeholder="What needs to be done?"
+          className="me-2"
+          name="name"
+          value={name}
+          onChange={this.handleChange}
+        />
         <Button variant="primary" type="submit">
           Add
         </Button>
