7e71e0f9d79d8058656e6618f65ec819ca25b12e
LordHippopotamus <LordHippopotamus@yandex.ru>
2021-10-10 07:47:36 +0300

    refactor(TodoListItem): get todo name in props

--- src/components/TodoListItem/index.jsx
+++ src/components/TodoListItem/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Button, ListGroup } from 'react-bootstrap';
 
-export default function TodoListItem() {
+export default function TodoListItem(props) {
+  const { name } = props;
   return (
     <ListGroup.Item className="d-flex align-items-center justify-content-between">
-      Todo item
+      {name}
       <Button variant="outline-success">
         Done
       </Button>
     </ListGroup.Item>
   );
 }
+
+TodoListItem.propTypes = {
+  name: PropTypes.string,
+};
+
+TodoListItem.defaultProps = {
+  name: 'Todo item',
+};
