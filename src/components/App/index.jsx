b67f481c2e9433c6eea2ab340b60fbc05b916514
LordHippopotamus <LordHippopotamus@yandex.ru>
2021-10-10 07:55:25 +0300

    refactor(App): transform to class сomponent

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -3,12 +3,19 @@ import { Container } from 'react-bootstrap';
 import TodoForm from '../TodoForm';
 import TodoList from '../TodoList';
 
-export default function App() {
-  return (
-    <Container className="bg-light p-4 my-5">
-      <TodoForm />
-      <hr className="my-3" />
-      <TodoList />
-    </Container>
-  );
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Container className="bg-light p-4 my-5">
+        <TodoForm />
+        <hr className="my-3" />
+        <TodoList />
+      </Container>
+    );
+  }
 }
