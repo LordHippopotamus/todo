4ddae096f7476b4776366a382416927e39698013
LordHippopotamus <LordHippopotamus@yandex.ru>
2021-10-10 10:25:08 +0300

    refactor(TodoForm): transform to class component

--- src/components/TodoForm/index.jsx
+++ src/components/TodoForm/index.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import { Form, Button } from 'react-bootstrap';
 
-export default function TodoForm() {
-  return (
-    <Form className="d-flex">
-      <Form.Control type="text" placeholder="What needs to be done?" className="me-2" />
-      <Button variant="primary" type="submit">
-        Add
-      </Button>
-    </Form>
-  );
+export default class TodoForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Form className="d-flex">
+        <Form.Control type="text" placeholder="What needs to be done?" className="me-2" />
+        <Button variant="primary" type="submit">
+          Add
+        </Button>
+      </Form>
+    );
+  }
 }
